fa1be796705dc5e16b206a7564b10bdf1c6bf7ba
YanakornS <654259002@webmail.npru.ac.th>
2024-09-27 21:20:34 +0700

    Web-App

--- Forntend/src/component/UserProfile.jsx
+++ Forntend/src/component/UserProfile.jsx
@@ -4,7 +4,7 @@ import { useNavigate, Link } from "react-router-dom";
 
 //Import รูปเข้ามา
 import Logout from "../assets/logout.png";
-import Profiles from "../assets/profiles.png";
+import Profile from "../assets/profiles.png";
 
 const UserProfile = () => {
   const { logout } = useAuthContext(); // ดึงฟังก์ชัน logout จาก context
@@ -42,7 +42,7 @@ const UserProfile = () => {
               className="flex items-center justify-between text-[#007BFF] hover:bg-[#F0F4FF] px-4 py-2 rounded-md"
             >
               Profile
-              <img src={Profiles} alt="Profiles Icon" className="w-5 h-5" />
+              <img src={Profile} alt="Profiles Icon" className="w-5 h-5" />
             </Link>
           </li>
 
